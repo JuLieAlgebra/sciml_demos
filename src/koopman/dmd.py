92aad8f8141473c004fa6cdb2cb1cedec60a4017
JuLieAlgebra <julieannabacon2014@gmail.com>
2023-05-17 15:28:41 -0400

    Added more description to docs

--- src/koopman/dmd.py
+++ src/koopman/dmd.py
@@ -6,15 +6,34 @@ import numpy as np
 
 import koopman.simulate
 
-
 def dmd(
     state_history: List[np.array], r: int = None
 ) -> Tuple[np.array, np.array, np.array]:
     """
-    Implements SVD-based dynamic mode decomposition.
+    Implements SVD-based Dynamic Mode Decomposition (DMD).
 
-    Assumes state_history[0] is an np.array of the state at time t = 0 and state_history[-1] grabs the the last
-    state.
+    Given a state history of the system, this function computes the DMD matrix and its corresponding eigenvalues
+    and eigenvectors. The DMD provides a low-dimensional approximation of the system dynamics and can be used for
+    forecasting, mode analysis, and control.
+
+    Args:
+        state_history: A list of numpy arrays representing the state history of the system.
+            Each numpy array represents the (flattened) state at a specific time step. The list should be ordered in time,
+            where `state_history[0]` represents the state at time t = 0 and `state_history[-1]` represents the
+            state at the final time step.
+        r: The rank truncation parameter for the DMD matrix. It determines the number of dominant modes to retain.
+            If not specified (None), the full-rank DMD matrix is computed.
+
+    Returns:
+        A tuple containing the computed DMD matrix, eigenvalues, and eigenvectors.
+        - koopman_matrix: The computed Koopman matrix representing the linear dynamics of the system.
+        - eigenvalues: The eigenvalues of the Koopman matrix.
+        - eigenvectors: The eigenvectors of the Koopman matrix.
+
+    Notes:
+        - The state history should have at least two time steps for DMD computation.
+        - The first time step in `state_history` corresponds to the initial condition, and the subsequent time steps
+          represent the evolution of the system.
     """
     state_history = np.array(state_history)
     last_state = state_history[:-1].copy()
--- src/koopman/visualizations.py
+++ src/koopman/visualizations.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+<<<<<<< HEAD:src/koopman/visualizations.py
 def plot_original_vs_dmd_reconstruction(original, A, eigenvalues, eigenvectors):
     original = np.array(original)
     x0 = original[0]
@@ -9,6 +10,40 @@ def plot_original_vs_dmd_reconstruction(original, A, eigenvalues, eigenvectors):
 
     # Initial condition projected into DMD modes
     b = np.linalg.pinv(eigenvectors) @ (A @ x0)
+=======
+def dmd(
+    state_history: List[np.array], r: int = None
+) -> Tuple[np.array, np.array, np.array]:
+    """
+    Implements SVD-based Dynamic Mode Decomposition (DMD).
+
+    Given a state history of the system, this function computes the DMD matrix and its corresponding eigenvalues
+    and eigenvectors. The DMD provides a low-dimensional approximation of the system dynamics and can be used for
+    forecasting, mode analysis, and control.
+
+    Args:
+        state_history: A list of numpy arrays representing the state history of the system.
+            Each numpy array represents the (flattened) state at a specific time step. The list should be ordered in time,
+            where `state_history[0]` represents the state at time t = 0 and `state_history[-1]` represents the
+            state at the final time step.
+        r: The rank truncation parameter for the DMD matrix. It determines the number of dominant modes to retain.
+            If not specified (None), the full-rank DMD matrix is computed.
+
+    Returns:
+        A tuple containing the computed DMD matrix, eigenvalues, and eigenvectors.
+        - koopman_matrix: The computed Koopman matrix representing the linear dynamics of the system.
+        - eigenvalues: The eigenvalues of the Koopman matrix.
+        - eigenvectors: The eigenvectors of the Koopman matrix.
+
+    Notes:
+        - The state history should have at least two time steps for DMD computation.
+        - The first time step in `state_history` corresponds to the initial condition, and the subsequent time steps
+          represent the evolution of the system.
+    """
+    state_history = np.array(state_history)
+    last_state = state_history[:-1].copy()
+    state = state_history[1:].copy()
+>>>>>>> 32bb1b0 (Added more description to docs):koopman/dmd.py
 
     # Time dynamics
     time = np.arange(timesteps)
@@ -91,6 +126,8 @@ def visualize_dmd_output(
 
     original_states = np.array(state_history)
     reconstructed_states = np.dot(eigenvectors, koopman_matrix)
+    print(reconstructed_states)
+    print(reconstructed_states.shape)
 
     plt.figure(figsize=(12, 6))
     plt.subplot(1, 2, 1)
